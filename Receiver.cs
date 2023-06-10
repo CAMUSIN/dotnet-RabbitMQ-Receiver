1f4bb67ddaeaeda942177876a2a24390babee067
CAMUSIN <fergg8704@gmail.com>
2023-06-10 11:13:24 -0600

    Adding code to use Exchange (Client-Suscriber)

--- Receiver.cs
+++ Receiver.cs
@@ -6,24 +6,42 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        //Create Factory - using RabbitMQDockerServer
         var factory = new ConnectionFactory() { HostName = "localhost" };
+        //Create Connection
         using (var connection = factory.CreateConnection())
         {
+            //Create channel to send/communicate
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "hello", durable: false,
-                    exclusive: false, autoDelete: false, arguments: null);
+                //Create queue with Exchange if not exist or use it if exist 
+                channel.ExchangeDeclare(exchange:"logs", type:ExchangeType.Fanout);
 
+                //Create queue if not exist or use it if exist
+                /*channel.QueueDeclare(queue: "hello", durable: false,
+                    exclusive: false, autoDelete: false, arguments: null);*/
+
+                //Getting queue name (random)
+                var queueName = channel.QueueDeclare().QueueName;
+
+                //Adding/Connecting 1 or N number of queues to the exchange channel
+                channel.QueueBind(queue:queueName, exchange:"logs", routingKey:"");
+                Console.WriteLine("waiting for logs..");
+                
+                //Listen to the Channel
                 var consumer = new EventingBasicConsumer(channel);
 
+                //Receive Event triggered
                 consumer.Received += (model, ea) => {
+                    //Getting body byte array
                     var body = ea.Body.ToArray();
+                    //String encoding
                     var message = Encoding.UTF8.GetString(body);
-
                     Console.WriteLine($"[x] Received {message}");
                 };
 
-                channel.BasicConsume(queue:"hello", autoAck:true, consumer:consumer);
+                //Configuring auto knowledge from queue to consumer
+                channel.BasicConsume(queue:queueName, autoAck:true, consumer:consumer);
 
                 Console.WriteLine("Press any key to exit...");
                 Console.Read();
